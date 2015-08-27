2cf9e80e0c59902645dd719e6343be70d9892fdd
Serghei Iakovlev <sadhooklay@gmail.com>
2015-08-28 02:35:30 +0300

    Unified log format

--- app/library/Listeners/Initializer.php
+++ app/library/Listeners/Initializer.php
@@ -6,13 +6,32 @@ use Phalcon\DiInterface;
 use Phalcon\Events\Manager as EventsManager;
 use Phalcon\Di\Injectable;
 use Phalcon\Events\Event;
+use Phalcon\Logger\Formatter\Line as FormatterLine;
 
+/**
+ * Class Initializer
+ * @package Cetraria\Library\Listeners
+ *
+ * @property \Phalcon\Config config
+ */
 class Initializer extends Injectable
 {
+    /**
+     * @var \Phalcon\Logger\Adapter\File
+     */
+    protected $logger;
+
     public function __construct(DiInterface $di, EventsManager $em)
     {
         $this->setDI($di);
         $this->setEventsManager($em);
+
+        $this->logger = $di->get('logger', ['init']);
+        $this->logger->setFormatter(new FormatterLine(
+                $di->get('config')->logger->format,
+                $di->get('config')->logger->date
+            )
+        );
     }
 
     public function beforeCache(Event $event, $source, $mode)
@@ -22,9 +41,7 @@ class Initializer extends Injectable
                 $source = get_class($source);
             }
 
-            /** @var \Phalcon\Logger\Adapter\File $logger */
-            $logger = $this->di->get('logger', ['init']);
-            $logger->debug(sprintf('%s: Init cache from %s as %s mode', $event->getType(), $source, $mode));
+            $this->logger->debug(sprintf('%s: Init cache from %s as %s mode', $event->getType(), $source, $mode));
         }
 
         return true;
