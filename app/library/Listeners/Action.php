f4d5429775f5f822b4087b635088377ede139a3e
Serghei Iakovlev <sadhooklay@gmail.com>
2015-08-28 09:05:00 +0300

    Created Action listener

---
+++ app/library/Listeners/Action.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Cetraria\Library\Listeners;
+
+use Phalcon\Events\Event;
+use Phalcon\Mvc\Dispatcher;
+
+/**
+ * Action listener
+ *
+ * @package Cetraria\Library\Listeners
+ */
+class Action extends Base
+{
+    /**
+     * @param Event      $event
+     * @param Dispatcher $dispatcher
+     */
+    public function beforeExecuteRoute(Event $event, Dispatcher $dispatcher)
+    {
+        if ($this->debug) {
+            $message = sprintf(
+                '%s::%s() started.',
+                get_class($dispatcher->getActiveController()),
+                $dispatcher->getActiveMethod()
+            );
+
+            $this->logger->debug($message);
+        }
+    }
+
+    /**
+     * @param Event      $event
+     * @param Dispatcher $dispatcher
+     */
+    public function afterExecuteRoute(Event $event, Dispatcher $dispatcher)
+    {
+        if ($this->debug) {
+            $message = sprintf(
+                '%s::%s() finished.',
+                get_class($dispatcher->getActiveController()),
+                $dispatcher->getActiveMethod()
+            );
+
+            $this->logger->debug($message);
+        }
+    }
+}
