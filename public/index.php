4d5cc9254f9c3194ec3cf16cc9514ea9c9c130c7
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-04 00:58:31 +0300

    Cleaned entry point

--- public/index.php
+++ public/index.php
@@ -5,9 +5,4 @@ use Cetraria\Library\Application;
 require realpath(dirname(dirname(__FILE__))) . '/config/env.php';
 
 $application = new Application;
-$application->init();
-
-$di = $application->getDI();
-
-var_dump($application);
-var_dump(class_exists('Abc\Cde'));
+$application->init()->run();
