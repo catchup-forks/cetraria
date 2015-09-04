c1db132a6d26ed7c7c8300c944b411f46ce09c3d
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-04 23:15:48 +0300

    Added initial base Module

---
+++ app/library/Mvc/Module.php
@@ -0,0 +1,161 @@
+<?php
+
+/*
+ +------------------------------------------------------------------------+
+ | Cetraria                                                               |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2015 Serghei Iakovlev                                    |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to me@klay.me so I can send you a copy immediately.                    |
+ +------------------------------------------------------------------------+
+*/
+
+namespace Cetraria\Library\Mvc;
+
+use Cetraria\Library\DiInjector;
+use Phalcon\Mvc\View;
+use Phalcon\DiInterface;
+use Phalcon\Mvc\View\Engine\Volt;
+use Phalcon\Mvc\ModuleDefinitionInterface;
+use Phalcon\Exception;
+use Phalcon\Mvc\View\Exception as ViewException;
+
+abstract class Module implements ModuleDefinitionInterface
+{
+    use DiInjector {
+        DiInjector::__construct as injectDi;
+    }
+
+    /**
+     * Module name
+     * @var string|null
+     */
+    protected $moduleName = null;
+
+    /**
+     * Views directory
+     * @var string|null
+     */
+    protected $viewsDirectory = null;
+
+    /**
+     * Module directory
+     * @var string|null
+     */
+    protected $moduleDirectory = null;
+
+    /**
+     * Module Constructor
+     *
+     * @throws \Phalcon\Exception
+     */
+    public function __construct()
+    {
+        $this->injectDi();
+
+        $this->moduleDirectory = $this->getDI()
+                ->get('registry')
+                ->directories
+                ->modules . ucfirst($this->getModuleName()) . '/';
+
+        $this->viewsDirectory = $this->getModuleDirectory() . 'Views/';
+    }
+
+    /**
+     * Registers services related to the module
+     *
+     * @param DiInterface $di
+     * @throws \Phalcon\Exception
+     * @throws \Phalcon\Mvc\View\Exception
+     */
+    public function registerServices(DiInterface $di)
+    {
+        $viewsDirectory = $this->viewsDirectory;
+
+        $di->setShared('view', function () use ($di, $viewsDirectory) {
+            $view   = new View;
+            $config = $di->get('config');
+            $em     = $di->get('eventsManager');
+
+            $view->registerEngines([
+                '.volt'  => function ($view, $di) use ($config) {
+                    $volt   = new Volt($view, $di);
+                    $config = $config->get('volt')->toArray();
+
+                    $options = [
+                        'compiledPath'      => $config['cacheDir'],
+                        'compiledExtension' => $config['compiledExt'],
+                        'compiledSeparator' => $config['separator'],
+                        'compileAlways'     => ENV_DEVELOPMENT === APPLICATION_ENV && $config['forceCompile'],
+                    ];
+
+                    $volt->setOptions($options);
+
+                    return $volt;
+                },
+                '.phtml' => 'Phalcon\Mvc\View\Engine\Php'
+            ]);
+
+            $view
+                ->setViewsDir($viewsDirectory)
+                ->disableLevel([View::LEVEL_LAYOUT => true]);
+
+            $em->attach('view', function ($event, $view) use ($di, $config) {
+                /**
+                 * @var \Phalcon\Logger\AdapterInterface $logger
+                 * @var \Phalcon\Mvc\View $view
+                 * @var \Phalcon\Events\Event $event
+                 */
+                if ($config->application->debug) {
+                    $logger = $di->get('logger', ['view']);
+                    $logger->debug(sprintf('Event %s. Path: %s', $event->getType(), $view->getActiveRenderPath()));
+                }
+
+                if ('notFoundView' == $event->getType()) {
+                    $message = sprintf('View not found: %s', $view->getActiveRenderPath());
+                    throw new ViewException($message);
+                }
+            });
+
+            $view->setEventsManager($em);
+
+            return $view;
+        });
+    }
+
+    /**
+     * Get module directory
+     *
+     * @return string
+     */
+    public function getModuleDirectory()
+    {
+        return $this->moduleDirectory;
+    }
+
+    public function getViewDirectory()
+    {
+        return $this->viewsDirectory;
+    }
+
+    /**
+     * Get module name
+     *
+     * @return null|string
+     * @throws \Phalcon\Exception
+     */
+    public function getModuleName()
+    {
+        if (empty($this->moduleName)) {
+            $class = new \ReflectionClass($this);
+            throw new Exception('Module has no module name: ' . $class->getFileName());
+        }
+
+        return $this->moduleName;
+    }
+}
--- app/modules/Core/Module.php
+++ app/modules/Core/Module.php
@@ -1,14 +1,33 @@
 <?php
 
+/*
+ +------------------------------------------------------------------------+
+ | Cetraria                                                               |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2015 Serghei Iakovlev                                    |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to me@klay.me so I can send you a copy immediately.                    |
+ +------------------------------------------------------------------------+
+*/
+
 namespace Cetraria\Modules\Core;
 
-use Phalcon\Mvc\View;
 use Phalcon\DiInterface;
-use Phalcon\Mvc\View\Engine\Volt;
-use Phalcon\Mvc\ModuleDefinitionInterface;
+use Cetraria\Library\Mvc\Module as CetrariaModule;
 
-class Module implements ModuleDefinitionInterface
+class Module extends CetrariaModule
 {
+    /**
+     * Module name
+     * @var string
+     */
+    protected $moduleName = 'Core';
+
     /**
      * Registers an autoloader related to the module
      *
@@ -16,65 +35,5 @@ class Module implements ModuleDefinitionInterface
      */
     public function registerAutoloaders(DiInterface $di = null)
     {
-    }
-
-    /**
-     * Registers services related to the module
-     *
-     * @param DiInterface $di
-     */
-    public function registerServices(DiInterface $di)
-    {
-        $di->setShared('view', function () use ($di) {
-            $view   = new View;
-
-            $config    = $di->get('config');
-            $em        = $di->get('eventsManager');
-            $directory = $di->get('registry')->directories->modules . 'Core/';
-
-            $view->registerEngines([
-                '.volt'  => function ($view, $di) use ($config) {
-                    $volt   = new Volt($view, $di);
-                    $config = $config->get('volt')->toArray();
-
-                    $options = [
-                        'compiledPath'      => $config['cacheDir'],
-                        'compiledExtension' => $config['compiledExt'],
-                        'compiledSeparator' => $config['separator'],
-                        'compileAlways'     => ENV_DEVELOPMENT === APPLICATION_ENV,
-                    ];
-
-                    $volt->setOptions($options);
-
-                    return $volt;
-                },
-                '.phtml' => 'Phalcon\Mvc\View\Engine\Php'
-            ]);
-
-            $view
-                ->setViewsDir($directory . 'Views/')
-                ->disableLevel([View::LEVEL_LAYOUT => true]);
-
-            $em->attach('view', function ($event, $view) use ($di, $config) {
-                /**
-                 * @var \Phalcon\Logger\AdapterInterface $logger
-                 * @var \Phalcon\Mvc\View $view
-                 * @var \Phalcon\Events\Event $event
-                 */
-                if ($config->application->debug) {
-                    $logger = $di->get('logger', ['view']);
-                    $logger->debug(sprintf('Event %s. Path: %s', $event->getType(), $view->getActiveRenderPath()));
-                }
-
-                if ('notFoundView' == $event->getType()) {
-                    $message = sprintf('View not found: %s', $view->getActiveRenderPath());
-                    throw new \Exception($message);
-                }
-            });
-
-            $view->setEventsManager($em);
-
-            return $view;
-        });
     }
 }
