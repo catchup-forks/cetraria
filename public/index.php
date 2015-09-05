b4d640a9acac1b0937939b0fbaa7f7a788c51904
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-05 12:09:14 +0300

    Optimized autoloading

--- public/index.php
+++ public/index.php
@@ -4,5 +4,9 @@ use Cetraria\Library\Application;
 
 require realpath(dirname(dirname(__FILE__))) . '/config/env.php';
 
+// Include App & Initializer
+require_once BASE_DIR . 'app/library/Initializer.php';
+require_once BASE_DIR . 'app/library/Application.php';
+
 $application = new Application;
-$application->init()->run();
+echo $application->init()->run();
