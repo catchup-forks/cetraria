d1f0ad51c5b7f2d5406c9f5377aa168f04bed37b
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-05 18:43:13 +0300

    Added error output

--- app/modules/Core/Controllers/ErrorController.php
+++ app/modules/Core/Controllers/ErrorController.php
@@ -1,13 +1,54 @@
 <?php
 
+/*
+ +------------------------------------------------------------------------+
+ | Cetraria                                                               |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2015 Serghei Iakovlev                                    |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to me@klay.me so I can send you a copy immediately.                    |
+ +------------------------------------------------------------------------+
+*/
+
 namespace Cetraria\Modules\Core\Controllers;
 
 use Phalcon\Mvc\Controller;
 
+/**
+ * Error Controller
+ * @package Cetraria\Modules\Core\Controllers
+ */
 class ErrorController extends Controller
 {
     public function indexAction()
     {
+        $error = $this->dispatcher->getParam('error');
 
+        switch ($error->code()) {
+            case 404:
+                $code = 404;
+                break;
+            case 403:
+                $code = 403;
+                break;
+            case 401:
+                $code = 401;
+                break;
+            default:
+                $code = 500;
+        }
+
+        $this->getDi()->getShared('response')->resetHeaders()->setStatusCode($code, null);
+
+        $this->view->setVars([
+            'code'  => $code,
+            'error' => $error,
+            'debug' => ENV_DEVELOPMENT === APPLICATION_ENV
+        ]);
     }
 }
--- app/modules/Core/Views/error/index.volt
+++ app/modules/Core/Views/error/index.volt
@@ -1,1 +1,11 @@
 {{ content() }}
+
+<h1>Error {{ code }}</h1>
+
+{{ error.message() }}
+{% if debug %}
+    <br>in {{ error.file() }} on line {{ error.line() }}<br>
+    {% if error.isException() %}
+        <pre>{{ error.exception().getTraceAsString() }}</pre>
+    {% endif %}
+{% endif %}
