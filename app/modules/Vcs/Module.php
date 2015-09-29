4e21f072418b3208bebf03eb1a388a9c704ca535
Serghei Iakovlev <sadhooklay@gmail.com>
2015-09-29 23:42:59 +0300

    Initial Vcs module

---
+++ app/modules/Vcs/Module.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ +------------------------------------------------------------------------+
+ | Cetraria                                                               |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2015 Serghei Iakovlev                                    |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to me@klay.me so I can send you a copy immediately.                    |
+ +------------------------------------------------------------------------+
+*/
+
+namespace Cetraria\Modules\Vcs;
+
+use Phalcon\DiInterface;
+use Cetraria\Library\Mvc\Module as CetrariaModule;
+
+class Module extends CetrariaModule
+{
+    /**
+     * Module name
+     * @var string
+     */
+    protected $moduleName = 'Vcs';
+
+    /**
+     * Registers an autoloader related to the module
+     *
+     * @param DiInterface $di
+     */
+    public function registerAutoloaders(DiInterface $di = null)
+    {
+    }
+}
